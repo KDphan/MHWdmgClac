a7fbfb6325e9c0ba33e0ffa3f8bd25b915f6e7c1
EC2 Default User <ec2-user@ip-172-31-27-236.us-east-2.compute.internal>
2019-10-04 17:51:12 +0000

    added flask and started refactoring for web app

--- MHWtruDMG.py
+++ MHWtruDMG.py
@@ -2,7 +2,8 @@
 
 import math
 import sys
-
+from flask import Flask, request, render_template, redirect, url_for
+import os
 
 
 def re_calc():
@@ -17,25 +18,28 @@ def re_calc():
                 
         
             
-def dmg_calc():   
-    DMG  = int(input('Enter your weapon damage value:'))
-    if DMG <= 0:
-          print('Invalid damage values, Hunter-san')
-          return dmg_calc()
+def dmg_calc():
+    WEAP = request.form['weap']
+    DMG  = request.form['dmg']
+    ELEM = request.form['edmg']
+    SHRP = request.form['sharp']
+    # if DMG <= 0:
+    #       print('Invalid damage values, Hunter-san')
+    #       return dmg_calc()
   
-    hammer  = DMG // 5.2
-    horn    = DMG // 4.2
-    SA      = DMG // 3.5
-    GS      = DMG // 4.8
-    CB      = DMG // 3.6
-    LS      = DMG // 3.3
-    IG      = DMG // 3.1
-    LanceGL = DMG // 2.3
-    HBG     = DMG // 1.5
-    SNS     = DMG // 1.4
-    DB      = DMG // 1.4
-    LBG     = DMG // 1.3
-    Bow     = DMG // 1.2  
+    hammer  = float(DMG) // 5.2
+    horn    = float(DMG) // 4.2
+    SA      = float(DMG) // 3.5
+    GS      = float(DMG) // 4.8
+    CB      = float(DMG) // 3.6
+    LS      = float(DMG) // 3.3
+    IG      = float(DMG) // 3.1
+    LanceGL = float(DMG) // 2.3
+    HBG     = float(DMG) // 1.5
+    SNS     = float(DMG) // 1.4
+    DB      = float(DMG) // 1.4
+    LBG     = float(DMG) // 1.3
+    Bow     = float(DMG) // 1.2  
   
   #Sharpness Modifiers for raw damage
     green   = 1.05
@@ -51,31 +55,44 @@ def dmg_calc():
           
     sharpness  = {'green': green, 'blue': blue, 'white': white}
     Esharpness = {'green': Egreen, 'blue': Eblue, 'white': Ewhite}
-  
-    def weap_calc():
-        WEAP = input('What is your weapon?').casefold()
-        if WEAP not in weapons:
-                print('invalid weapon, try inputting: hammer, horn, SA, GS, CB, LS, IG, LanceGL, HBG, SNS, DB, LBG, Bow')
-                return weap_calc()
-        else:
-              ELEM    = int(input('What is your element damage?'))
-              SHRP    = input('What is your sharpness color?')
-              while SHRP not in sharpness:
-                print('Invalid color, try green, blue, or white')
-                SHRP    = input('What is your sharpness color?')
-              else:
-                eleSHRP = SHRP
-                truDmg = weapons[WEAP]
-                eleDmg = ELEM // 10
-                truEleDmg = truDmg + eleDmg
-                truEleShrpDmg = (truDmg*sharpness[SHRP]) + (eleDmg*Esharpness[eleSHRP])
+    
+    eleSHRP = SHRP
+    truDmg = weapons[WEAP]
+    eleDmg = float(ELEM) // 10
+    truEleDmg = truDmg + eleDmg
+    truEleShrpDmg = (truDmg*sharpness[SHRP]) + (eleDmg*Esharpness[eleSHRP])
+    
+    damage_result = (" Weapon: " + str(WEAP) + 
+                    " \nDamage: " + str(truDmg) + 
+                    " \nElement: " + str(eleDmg) + 
+                    " \nTrue Damage: " + str(truEleShrpDmg))
+    
+    return damage_result
+      
+    # def weap_calc():
+    #     WEAP = input('What is your weapon?').casefold()
+    #     if WEAP not in weapons:
+    #             print('invalid weapon, try inputting: hammer, horn, SA, GS, CB, LS, IG, LanceGL, HBG, SNS, DB, LBG, Bow')
+    #             return weap_calc()
+    #     else:
+    #           ELEM    = int(input('What is your element damage?'))
+    #           SHRP    = input('What is your sharpness color?')
+    #           while SHRP not in sharpness:
+    #             print('Invalid color, try green, blue, or white')
+    #             SHRP    = input('What is your sharpness color?')
+    #           else:
+    #             eleSHRP = SHRP
+    #             truDmg = weapons[WEAP]
+    #             eleDmg = ELEM // 10
+    #             truEleDmg = truDmg + eleDmg
+    #             truEleShrpDmg = (truDmg*sharpness[SHRP]) + (eleDmg*Esharpness[eleSHRP])
                 
-              print('The true RAW damage for your', WEAP, 'is: ', truDmg)
-              print('The true RAW+ELEM damage for your', WEAP, 'is: ', truEleDmg)    
-              print('The true RAW+ELEM+SHARP damage for your', WEAP, 'is ', truEleShrpDmg)
-        re_calc()
-    weap_calc()
-dmg_calc()
+    #           print('The true RAW damage for your', WEAP, 'is: ', truDmg)
+    #           print('The true RAW+ELEM damage for your', WEAP, 'is: ', truEleDmg)    
+    #           print('The true RAW+ELEM+SHARP damage for your', WEAP, 'is ', truEleShrpDmg)
+        #re_calc()
+    #weap_calc()
+#dmg_calc()
     
  
 
